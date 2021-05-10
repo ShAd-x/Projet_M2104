85dea483da90f77969a0ae5a9c336859c837e67f
ShAd-x <riValofking@gmail.com>
2021-05-10 18:41:33 +0200

    création de deux classes: décodage et codage.

---
+++ Projet/Decodage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Projet
+{
+    class Decodage
+    {
+        public void Decode(string cheminFichier)
+        {
+            try
+            {
+                string str;
+                StreamReader sr = new StreamReader(cheminFichier);
+                while ((str = sr.ReadLine()) != null)
+                {
+                    string[] T = str.Split(':', '|');
+                    Carte Map = new Carte(T);
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+    }
+}

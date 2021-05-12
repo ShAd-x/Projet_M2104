21a5d4c65eaf41e41a5d8ae7dc8a0b7ee1ba51dc
damien.harle@gmail.com <damien.harle@gmail.com>
2021-05-12 10:40:31 +0200

    oui

--- Projet/Codage.cs
+++ Projet/Codage.cs
@@ -7,5 +7,39 @@ namespace Projet
 {
     class Codage
     {
+        public Codage(string cheminFichier)
+        {
+            try
+            {
+                const int longueur = 10;
+                const int largeur = 10;
+                int code;
+
+                int[,] transi = new int[longueur, largeur];
+                string str;
+                StreamReader sr = new StreamReader(cheminFichier);
+                while ((str = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        char Char = str[i];
+                        
+                        if (Char == 'M')
+                        {
+                            code += 64;
+                        } else if (Char == 'F')
+                        {
+                            code += 32;
+                        }
+                    }
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
     }
 }
